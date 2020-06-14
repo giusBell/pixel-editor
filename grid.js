2c8434fe7effc5548ac5725a6939f7c84725359c
Giuseppe Bellomo <giuseppe.bellomo@alten.it>
2020-06-14 14:48:39 +0200

    adds printSquare function

--- grid.js
+++ grid.js
@@ -1,12 +1,11 @@
-// takes a node and prints a grid in a html canvas
-
-export const print = (canvasEl, rows) => {
+// takes a canvas ID and prints a grid
+export const printGrid = (canvasEl, rows) => {
   const canvas = document.getElementById(canvasEl);
   const ctx = canvas.getContext('2d');
   const cWidth = canvas.offsetWidth;
   const cHeight = canvas.offsetHeight;
-  let xSpan = cWidth / rows;
-  let ySpan = cWidth / rows;
+  let span = cWidth / rows;
+
   ctx.rect(0, 0, cWidth, cHeight);
   ctx.save();
   if (rows / 2 === parseInt(rows / 2)) {
@@ -14,8 +13,8 @@ export const print = (canvasEl, rows) => {
   }
   ctx.beginPath();
   for (let i = 0; i < rows; i++) {
-    let x = (i + 1) * xSpan;
-    let y = (i + 1) * ySpan;
+    let x = (i + 1) * span;
+    let y = (i + 1) * span;
     ctx.moveTo(x, 0);
     ctx.lineTo(x, cHeight);
     ctx.moveTo(0, y);
@@ -24,4 +23,24 @@ export const print = (canvasEl, rows) => {
   ctx.lineWidth = 0.5;
   ctx.stroke();
   ctx.restore();
+};
+
+// takes a canvas ID, the rows, the color and prints a square
+export const printSquare = (canvasEl, rows, color) => {
+  const canvas = document.getElementById(canvasEl);
+  const ctx = canvas.getContext('2d');
+  const cWidth = canvas.offsetWidth;
+  const span = cWidth / rows;
+
+  // getting canvas coordinates of click
+  let rect = canvas.getBoundingClientRect();
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+
+  // calculate the coordinate of the square
+  x = (Math.ceil(x / span) - 1) * span;
+  y = (Math.ceil(y / span) - 1) * span;
+
+  ctx.fillStyle = '#FF0000';
+  ctx.fillRect(x, y, span, span);
 };
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { a } from './moduleA';
-import { print } from './grid';
+import { printGrid, printSquare } from './grid';
 
 console.log(a);
-const CANVAS = 'canvas-grid';
 
-print(CANVAS, 16);
+const CANVAS_ID = 'canvas-grid';
+const canvasEl = document.getElementById(CANVAS_ID);
+
+printGrid(CANVAS_ID, 32);
+canvasEl.addEventListener('click', () => printSquare(CANVAS_ID, 32, 'black'));
